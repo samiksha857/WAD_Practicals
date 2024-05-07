637ea3de17f9f8533a152d02982c46356c1d736a
dell laptop <dsbdasamiksha@gmail.com>
2024-05-07 09:12:48 +0530

    book-store added

---
+++ Book-Store-MERN-Stack-main/Book-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://samiksha:1234@cluster0.xltlpv6.mongodb.net/?retryWrites=true';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
